5c6aba1870d84e639f56df3794f3b046c1b32a05
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-03-27 10:42:24 -0700

    Initialize project, add CloudKit controller

---
+++ PicFeed/PicFeed/CloudKit.swift
@@ -0,0 +1,22 @@
+//
+//  CloudKit.swift
+//  PicFeed
+//
+//  Created by Robert Hatfield on 3/27/17.
+//  Copyright © 2017 Robert Hatfield. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+class CloudKit {
+    
+    static let shared = CloudKit()
+    
+    let container = CKContainer.default()
+    
+    var privateDatabase : CKDatabase {
+        return container.privateCloudDatabase
+    }
+    
+}
