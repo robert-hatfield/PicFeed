5e6323d1062fffd2d3545ab3618cd374b0d82f4b
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-03-28 13:37:08 -0700

    Define filter method

--- PicFeed/PicFeed/Filters.swift
+++ PicFeed/PicFeed/Filters.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-enum FilterNamed : String {
+enum FilterName : String {
     case vintage = "CIPhotoEffectTransfer"
     case blackAndWhite = "CIPhotoEffectMono"
 }
@@ -19,4 +19,30 @@ class Filters {
     
     static var originalImage = UIImage()
     
+    class func filter(name: FilterName, image: UIImage, completion: @ escaping FilterCompletion) {
+        OperationQueue().addOperation {
+            guard let filter = CIFilter(name: name.rawValue) else { fatalError("Failed to create CIFilter") }
+            let coreImage = CIImage(image: image)
+            filter.setValue(coreImage, forKey: kCIInputImageKey)
+            
+            // GPU Context
+            let options = [kCIContextWorkingColorSpace: NSNull()]
+            guard let eaglContext = EAGLContext(api: .openGLES2) else { fatalError("Failed to create EAGLContext") }
+            let ciContext = CIContext(eaglContext: eaglContext, options: options)
+            
+            // Get filtered image from GPU
+            guard let outputImage = filter.outputImage else { fatalError("Failed to get output Image from filter") }
+            
+            if let cgImage = ciContext.createCGImage(outputImage, from: outputImage.extent) {
+                let finalImage = UIImage(cgImage: cgImage)
+                OperationQueue.main.addOperation {
+                    completion(finalImage)
+                }
+            } else {
+                OperationQueue.main.addOperation {
+                    completion(nil)
+                }
+            }
+        }
+    }
 }
