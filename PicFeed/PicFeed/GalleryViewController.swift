63a5b7d78dab1369b9379d922d95fee14fe1578a
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-03-30 11:44:55 -0700

    Add a delegate protocol to GalleryViewController

--- PicFeed/PicFeed/GalleryViewController.swift
+++ PicFeed/PicFeed/GalleryViewController.swift
@@ -8,8 +8,13 @@
 
 import UIKit
 
-class GalleryViewController: UIViewController {
+protocol GalleryViewControllerDelegate : class {
+    func galleryController(didSelect image: UIImage)
+}
 
+class GalleryViewController: UIViewController {
+    weak var delegate : GalleryViewControllerDelegate?
+    
     @IBOutlet weak var collectionView: UICollectionView!
     
     var allPosts = [Post]() {
@@ -22,6 +27,7 @@ class GalleryViewController: UIViewController {
         super.viewDidLoad()
         
         self.collectionView.dataSource = self
+        collectionView.delegate = self
         self.collectionView.collectionViewLayout = GalleryCollectionViewLayout(columns: 2)
     }
     
@@ -43,7 +49,7 @@ class GalleryViewController: UIViewController {
 
 
 //MARK: UICollectionViewDataSource Extension
-extension GalleryViewController : UICollectionViewDataSource {
+extension GalleryViewController : UICollectionViewDataSource, UICollectionViewDelegate {
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: GalleryCell.identifier, for: indexPath) as! GalleryCell
@@ -55,6 +61,13 @@ extension GalleryViewController : UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return allPosts.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard let delegate = self.delegate else { return }
+        
+        let selectedPost = self.allPosts[indexPath.row]
+        delegate.galleryController(didSelect: selectedPost.image)
     }
     
 }
